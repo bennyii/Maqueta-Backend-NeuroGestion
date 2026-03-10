b84f6228a48e2d6865214367de62affe91eea034
Benjamin <benjaminn.ignacioo135@gmail.com>
2026-03-10 02:06:48 -0300

    PROGRESO DE LA PERSONA (CON ID)

--- src/routes/personas.ts
+++ src/routes/personas.ts
@@ -4,10 +4,14 @@ import { getPersonasByRol } from "../controllers/personas";
 import express from 'express';
 import { isAdmin } from "../middlewares/auth.middleware";
 import { requireAuth } from "../middlewares/auth.middleware";
+import { get } from "node:http";
+import { getProgresoByPersona } from "../controllers/progreso";
 
 const router = express.Router();
 
 router.get('/personas', getPersonas);
 router.get('/personas_rol', requireAuth, isAdmin,getPersonasByRol);
+router.get('/progreso_persona/:id', getProgresoByPersona);
+//router.get('/progreso_persona/:id',  getProgresoByPersona);
 
 export default router;
